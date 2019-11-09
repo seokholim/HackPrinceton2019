efb7124cbd7f58c7854226c1df2b600de9914976
Srinidhi Emkay <emkay@wisc.edu>
2019-11-09 18:33:04 -0500

    Add ReadSMS, and getCordinates

--- nodeapi/index.js
+++ nodeapi/index.js
@@ -4,8 +4,44 @@
  * @param {!express:Request} req HTTP request context.
  * @param {!express:Response} res HTTP response context.
  */
-exports.helloWorld = (req, res) => {
-  let message = req.query.message || req.body.message || 'Hello World!';
-  res.status(200).send(message);
+exports.readSMS = (req, res) => {
+  let obj = new Object();
+
+  let phone = req.body.phone;
+  let message = req.body.message.split(";");
+  let start_time = message.pop();
+  obj.end_loc = message.pop();
+  obj.start_loc = message.pop();
+  let isDriver = message.pop() == "D" ? true : false;
+
+  // TODO
+  obj.end_loc = getCordinates(end_loc);
+  obj.start_loc = getCordinates(start_loc);
+
+  res.status(200).json(JSON.stringify(obj));
 };
 
+function getCordinates(loc){
+  /json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
+
+  var request = new XMLHttpRequest()
+
+  // Open a new connection, using the GET request on the URL endpoint
+  request.open('POST', 'https://maps.googleapis.com/maps/api/geocode', true)
+
+  request.onload = function() {
+    // Begin accessing JSON data here
+    'address' : loc
+    'key' : 'AIzaSyD255iu19fCeI7Tzsz-cWWTmkmXdfmpfOI'
+  }
+
+  // Send request
+  request.send()
+  return request;
+}
+
+exports.ConfirmationRide = (req, res) => {
+  // Add Rider and Driver to Pairing
+  // Increase Booked Spots +1
+  // Send SMS Booked Ride
+};
