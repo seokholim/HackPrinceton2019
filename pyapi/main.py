317ddc9d43d64aec7c268bffdb13e4739d13ff8f
Srinidhi Emkay <emkay@wisc.edu>
2019-11-09 23:50:03 -0500

    Change to python!

--- pyapi/main.py
+++ pyapi/main.py
@@ -1,6 +1,22 @@
-import requests
+from flask import Flask, request #import main Flask class and request object
 
-def ReadSMS(request):
+from flask_sqlalchemy import SQLAlchemy
+import requests, googlemaps, json, sqlalchemy, datetime, os
+
+app = Flask(__name__) #create the Flask app
+gmaps = googlemaps.Client(key='AIzaSyD255iu19fCeI7Tzsz-cWWTmkmXdfmpfOI')
+
+# [START gae_flex_postgres_app]
+# Environment variables are defined in app.yaml.
+print(os.environ)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:Password99@/postgres?host=/cloudsql/hackprinceton-258521:us-east1:hackprinceton' #os.environ['SQLALCHEMY_DATABASE_URI']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def ReadSMS():
     """Responds to any HTTP request.
     Args:
         request (flask.Request): HTTP request object.
@@ -9,31 +25,28 @@ def ReadSMS(request):
         Response object using
         `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
     """
-    request_json = request.get_json()
+    request_json = request.json
     phone = request_json['phone']
-    message = request_json['message']
-    message = body.message.split(";")
-    start_time = message[3]
-    end_loc = message[2]
-    start_loc = message[1]
-
-    isDriver = 'true' if message[0] == "D" else 'false'
+    num_passengers = request_json['num_passengers']
+    start_time = request_json['start_time']
+    end_loc = request_json['end_loc']
+    start_loc = request_json['start_loc']
+    isDriver = request_json['is_driver']
 
     start_loc = getCordinates(start_loc)
     end_loc = getCordinates(end_loc)
 
+    return end_loc
+
 
 def getCordinates(loc):
-    url = 'https://maps.googleapis.com/maps/api/geocode?key=AIzaSyD255iu19fCeI7Tzsz-cWWTmkmXdfmpfOI'
-    params = dict(
-    address=loc
-    )
-    resp = requests.get(url=url, params=params)
-    return resp.json()
+    geocode_result = geocode_result = gmaps.geocode(loc)
+    coordinates = geocode_result[0]["geometry"]["location"]
 
+    return json.dumps(coordinates)
 
-def hello_world(request):
-  # Add Rider and Driver to Pairing
-  # Increase Booked Spots +1
-  # Send SMS Booked Ride
-  x = 4
+def pathCalc(userid, start, end, time, seats):
+    return 'todo'
+
+if __name__ == "__main__":
+    app.run(debug=True)
