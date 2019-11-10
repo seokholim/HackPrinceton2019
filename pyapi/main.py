08fcb2fc0cf91476c5353f6809d4361606698238
Srinidhi Emkay <emkay@wisc.edu>
2019-11-10 05:42:55 -0500

    message

--- pyapi/main.py
+++ pyapi/main.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine, text
 import requests, googlemaps, json, os
 import sqlalchemy as db
 from datetime import datetime, timedelta
+import time
 
 app = Flask(__name__) #create the Flask app
 gmaps = googlemaps.Client(key='AIzaSyD255iu19fCeI7Tzsz-cWWTmkmXdfmpfOI')
@@ -39,7 +40,7 @@ def ReadSMS():
 
 
 def getCordinates(loc):
-    geocode_result = geocode_result = gmaps.geocode(loc)
+    geocode_result = gmaps.geocode(loc)
     coordinates = geocode_result[0]["geometry"]["location"]
 
     return coordinates
@@ -48,16 +49,17 @@ def pathCalc(phone, start, end, time, seats):
     timesub1h = datetime.strptime(time,'%Y-%m-%d %H:%M') - timedelta(hours=1)
     timeadd1h = datetime.strptime(time,'%Y-%m-%d %H:%M') + timedelta(hours=1)
 
-    QUERY = """SELECT path_id, start_log, start_lat, end_log, end_lat, start_time FROM paths WHERE start_time > :timesub1h AND start_time < :timeadd1h ORDER BY (POW((start_log-:s_lng),2) + POW((start_lat-:s_lat),2)) + (POW((end_log-:e_lng),2) + POW((end_lat-:e_lat),2)) LIMIT 1"""
+    QUERY = """SELECT path_id, start_lat, start_log, end_lat, end_log, start_time FROM paths WHERE start_time > :timesub1h AND start_time < :timeadd1h ORDER BY (POW((start_log-:s_lng),2) + POW((start_lat-:s_lat),2)) + (POW((end_log-:e_lng),2) + POW((end_lat-:e_lat),2)) LIMIT 1"""
 
     ResultProxy = connection.execute(text(QUERY), timesub1h=timesub1h, timeadd1h=timeadd1h, s_lng=start['lng'], s_lat=start['lat'], e_lng=end['lng'], e_lat=end['lat'])
 
     ResultSet = ResultProxy.fetchall()
+    # time.sleep(2)
 
-    directions_result = gmaps.directions('{' + str(start['lng']) + ',' + str(start['lat']) + '}', '{' + str(ResultSet[0][1]) + ',' + str(ResultSet[0][2]) + '}' ,mode="transit",arrival_time=ResultSet[0][5])
+    #directions_result = gmaps.directions('{' + str(start['lat']) + ',' + str(start['lng']) + '}', '{' + str(ResultSet[0][1]) + ',' + str(ResultSet[0][2]) + '}' ,mode="transit",arrival_time=ResultSet[0][5])
 
-    print(str(directions_result))
-    return directions_result
+    #print(str(directions_result))
+    return str(ResultSet[0])
 
 if __name__ == "__main__":
     app.run(debug=True)
